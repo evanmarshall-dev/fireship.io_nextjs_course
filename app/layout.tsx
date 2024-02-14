1ece49dfb9a9a5219bb80a641ac090077d711c63
evanmarshall-dev <me@evanmarshall.dev>
2024-02-13 23:57:44 -0400

    FREELANCE: Starting by editing the layout.tsx to include a global nav element and main element wrapping all content. Also updated page.tsx in app dir to demo updating the main page hero element.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/projects">Projects</Link>
+            <Link href="/contact">Contact</Link>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
