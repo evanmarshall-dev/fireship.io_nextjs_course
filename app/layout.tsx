9dd344eef6040fce0cd75cbd75cdc0aeb052eee4
evanmarshall-dev <me@evanmarshall.dev>
2024-02-15 23:10:47 -0400

    FREELANCE: Starting my setup for linting, formatting, lint-staged and husky pre-commit.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import NavMenu from "../components/(navigation)/NavMenu";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import NavMenu from '../components/(navigation)/NavMenu';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Web Developer: Evan Marshall",
+  title: 'Web Developer: Evan Marshall',
   description:
-    "I design, build and maintain businesses online. Whether it is branding, updates, or full website rebuilds; I am your web developer. Contact me to discuss how you want to get your business working for you online.",
+    'I design, build and maintain businesses online. Whether it is branding, updates, or full website rebuilds; I am your web developer. Contact me to discuss how you want to get your business working for you online.',
 };
 
 export default function RootLayout({
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
         <NavMenu />
         {children}
